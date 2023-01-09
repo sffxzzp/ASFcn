e2521602caf3c255e6ff8bfff23bb566253a7cfd
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2023-01-09 16:56:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-echo '127.0.0.1 steamcommunity.com www.steamcommunity.com' >> /etc/hosts
+echo '127.0.0.1 steamcommunity.com www.steamcommunity.com store.steampowered.com' >> /etc/hosts
 caddy start --config /app/Caddyfile
 bash /app/ArchiSteamFarm-Service.sh --no-restart --process-required --system-required
