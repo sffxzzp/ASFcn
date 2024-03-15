e878783ef8c9cf2075a4962f8e5d4bdfe48614f2
sffxzzp <3407292+sffxzzp@users.noreply.github.com>
2024-03-15 14:53:07 +0800

    默认禁用升级和统计，取消 api.github.com 的本地反代

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-echo '127.0.0.1 steamcommunity.com www.steamcommunity.com store.steampowered.com api.github.com github.com' >> /etc/hosts
+echo '127.0.0.1 steamcommunity.com www.steamcommunity.com store.steampowered.com github.com' >> /etc/hosts
 if [ ! -f "/app/config/IPC.config" ]; then
     echo '{"Kestrel":{"Endpoints":{"HTTP":{"Url":"http://*:1242"}}}}' >> /app/config/IPC.config
 fi
 if [ ! -f "/app/config/ASF.json" ]; then
-    echo '{"IPCPassword":"asfcnasfcn"}' >> /app/config/ASF.json
+    echo '{"IPCPassword":"asfcnasfcn","UpdateChannel": 0,"UpdatePeriod": 0,"Statistics": false}' >> /app/config/ASF.json
 fi
 caddy start --config /app/Caddyfile
 bash /asf/ArchiSteamFarm-Service.sh --no-restart --process-required --system-required
